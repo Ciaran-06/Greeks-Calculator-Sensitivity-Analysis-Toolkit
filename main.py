dc434ce9c2470d295a36cf19e1f83cf2eba3a26c
Ciaran-06 <ciaran.gaffney@icloud.com>
2026-09-19 00:58:22 +0100

    addded git ignore for avoiding uploading data possibily with restrictions

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import numpy as np
+import pandas as pd
+import sys
 
-my_data = np.genfromtxt('my_file.csv', delimiter=',')
+if len(sys.argv) < 2:
+    print('Usage: python main.py <ticker>')
+    sys.exit(1)
+
+ticker_symbol = sys.argv[1].upper()
+
+df = pd.read_csv(f'./data/cleaned/{ticker_symbol}.csv')
+
+print(f"Loaded {len(df)} records for {ticker_symbol}")
+print(df.head(10))
