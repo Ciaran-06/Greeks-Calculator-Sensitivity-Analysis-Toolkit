e54071f60903c5210b6938af60360088002dad63
Ciaran-06 <ciaran.gaffney@icloud.com>
2026-09-19 04:18:19 +0100

    added delta and numerical delta check

--- greeks.py
+++ greeks.py
@@ -14,17 +14,19 @@ def calculate_bs(d1,d2,S,K,r,vol,T):
 def calculate_delta(d1):
     return norm.cdf(d1)
 
+
 if __name__ == "__main__":  
     S=100 
     K=100
     T=1
     r=0.05 
     vol =0.2 
+    
     #0.35
     d1 = calculate_d1(S,K,r,vol,T)
     d2 = calculate_d2(d1,T,vol)
-    delta = calculate_delta(d1)
     price = calculate_bs(d1, d2, S, K, r, vol, T)
+    delta = calculate_delta(d1)
     print(f'd_1: {d1} \nd_2: {d2} \ndelta: {delta}')
     S=110
     K=100 
@@ -37,5 +39,19 @@ if __name__ == "__main__":
     delta = calculate_delta(d1)
     price = calculate_bs(d1, d2, S, K, r, vol, T)
     print(f'd_1: {d1} \nd_2: {d2}\ndelta: {delta}')
+    epsilon = 0.01
     
+    S_up = S + epsilon
+    d1_up = calculate_d1(S_up, K, r, vol, T)
+    d2_up = calculate_d2(d1_up, T, vol)
+    price_up = calculate_bs(d1_up, d2_up, S_up, K, r, vol, T)
+
+    S_down = S - epsilon
+    d1_down = calculate_d1(S_down, K, r, vol, T)
+    d2_down = calculate_d2(d1_down, T, vol)
+    price_down = calculate_bs(d1_down, d2_down, S_down, K, r, vol, T)
+
+    num_delta = (price_up - price_down) / (2 * epsilon)
+    
+    print(f'Analytical delta: {delta}\nNumerical delta: {num_delta}')
     
