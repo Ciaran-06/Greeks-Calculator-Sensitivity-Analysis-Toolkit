7b18652917a61486557549a197ec12ea67475a2f
Ciaran Gaffney <ciaran.gaffney@icloud.com>
2026-09-22 15:04:10 +0100

    finished transfering for yahoo finance to databento flat files

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import databento as db
 
+from tabulate import tabulate
+
 import sys
 import csv
 import os
@@ -14,14 +16,7 @@ if len(sys.argv) < 2:
 print('Starting Data Ingestion...')
 ticker_symbol = sys.argv[1].upper()
 
-folder = Path('./data/uncleaned')
-new_folder = ticker_symbol
-new_path = folder / new_folder
 #db.DBNStore.from_file(path).to_df()
-
-if len(sys.argv) < 2:
-    print("Usage: python data_ingestion.py <ticker>")
-    sys.exit(1)
 
 ticker_symbol = sys.argv[1].upper()
 data = []     
@@ -33,8 +28,26 @@ for regime in ['low_vol', 'high_vol']:
         if filename.endswith('dbn.zst'):
             current_data = db.DBNStore.from_file(f'./data/unproccesed/{ticker_symbol}/{regime}/{filename}').to_df()
             current_data['regime'] = regime
+            current_data = current_data.sort_values('ts_event')
+            current_data['date'] = current_data['ts_event'].dt.normalize() 
+            current_data = current_data.drop_duplicates(subset=['symbol', 'date'], keep='last')
             data.append(current_data)
 
 df = pd.concat(data, ignore_index=True)
 
-print(df.columns)
+#Index(['ts_event', 'rtype', 'publisher_id', 'instrument_id', 'side', 'price',
+#       'size', 'flags', 'bid_px_00', 'ask_px_00', 'bid_sz_00', 'ask_sz_00',
+#       'bid_pb_00', 'ask_pb_00', 'symbol', 'regime'],
+#      dtype='str') 
+df['date'] = df['ts_event'].dt.tz_localize(None).dt.normalize()
+df['mid_price'] = (df['bid_px_00'] + df['ask_px_00']) / 2
+df['strike'] = (df['symbol'].str[-8:]).astype(float)/1000
+df['type'] = df['symbol'].str[-9]
+df['expiry'] = df['symbol'].str[-15:-9]
+df['expiry'] = pd.to_datetime(df['expiry'], format='%y%m%d')
+df['T'] = (df['expiry'] - df['date']).dt.days / 365
+print(df.head(10))
+print(len(df))    
+
+os.makedirs(f'./data/uncleaned/{ticker_symbol}', exist_ok=True)
+df.to_csv(f'./data/uncleaned/{ticker_symbol}/{ticker_symbol}.csv', index=False)
